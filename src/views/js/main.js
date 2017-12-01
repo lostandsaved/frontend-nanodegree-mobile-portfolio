e9c94867a3bf3271cfba21c5c79e04de75a5e588
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-12-01 15:35:19 -0500

    Update main.js

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -519,7 +519,7 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((pageTop / 1250) + (i % 5));
    // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    items[i].style.transform = "translateX(" + (600 * phase) + "px)";
+    items[i].style.transform = "translateX(" + (100 * phase) + "px)";
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
